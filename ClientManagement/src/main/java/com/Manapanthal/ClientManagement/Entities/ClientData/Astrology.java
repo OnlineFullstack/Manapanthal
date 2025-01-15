d74a1584436dcd1c25df33ff0b67891900359fdb
Piratha <piratha250392@yahoo.com>
2025-01-15 20:04:23 +0530

    ok

--- ClientManagement/src/main/java/com/Manapanthal/ClientManagement/Entities/ClientData/Astrology.java
+++ ClientManagement/src/main/java/com/Manapanthal/ClientManagement/Entities/ClientData/Astrology.java
@@ -6,10 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
-@Data
+@Getter
+@Setter
 @Entity
 public class Astrology extends DateTimeGenerate {
     @Id
